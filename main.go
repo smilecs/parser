2d1129e3ef908d66466bdf5c8182101cd9feac4c
smilecs <mumene@gmail.com>
2018-06-04 16:44:46 +0100

    added ability to categorise popular text(personaly related, for now)

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"encoding/xml"
 	"io/ioutil"
+	"regexp"
+	s "strings"
 
 	log "github.com/sirupsen/logrus"
 )
@@ -13,18 +15,21 @@ type Sms struct {
 	Address       string `xml:"address,attr"`
 	ServiceCenter string `xml:"service_center,attr"`
 	BankName      string
-	IsCredit      bool
 	IsDebit       bool
 	Cateogry      string
+	Amount        string
 }
 
 type MainSms struct {
 	SmsTag []Sms `xml:"sms"`
 }
 
+const DEBIT = "debit"
+
 func main() {
+	//log.SetFormatter(&log.JSONFormatter{})
 	sms := MainSms{}
-	//newSmsList := []Sms{}
+	// newSmsList := []Sms{}
 	data, err := ioutil.ReadFile("./sms.xml")
 	if err != nil {
 		log.Println(err)
@@ -33,6 +38,47 @@ func main() {
 	if err != nil {
 		log.Println(err)
 	}
+	for _, v := range sms.SmsTag {
+		alert, bankName := isAccountAlert(s.ToLower(v.Body))
+		v.BankName = bankName
+		if alert {
+			v.IsDebit = isDebit(s.ToLower(v.Body))
+			v.Cateogry = tagCategory(s.ToLower(v.Body))
+			log.Info(v)
+		}
 
-	log.Info(sms.SmsTag[0])
+	}
+}
+
+func isAccountAlert(value string) (bool, string) {
+	var bankName string
+	bankMatcher, _ := regexp.MatchString(`(\d{3}[A-Za-z]{4}\d{3})`, value)
+	for _, k := range BankNames {
+		if s.Contains(value, k) && bankMatcher {
+			bankName = k
+			break
+		}
+	}
+	return bankMatcher, bankName
+}
+
+func isDebit(body string) bool {
+	return s.Contains(body, DEBIT)
+}
+
+func tagCategory(body string) string {
+	var key = "other"
+	for k, v := range Categories {
+		for _, vv := range v {
+			if s.Contains(body, vv) {
+				key = k
+				break
+			}
+		}
+	}
+	return key
+}
+
+func getAmount() {
+
 }
---
+++ matcher.go
@@ -0,0 +1,31 @@
+package main
+
+var (
+	BankNames = map[string]string{
+		"1":  "firstbank",
+		"2":  "fcmb",
+		"3":  "uba",
+		"4":  "gtbank",
+		"5":  "keystone",
+		"6":  "unionbank",
+		"7":  "zenith",
+		"8":  "sterling",
+		"9":  "wema",
+		"10": "alat",
+	}
+
+	Categories = map[string][]string{
+		"eatery":        {"chicken republic", "sweet sensation", "uq restaurant", "kfc", "debonairs", "dominos", "cold stone"},
+		"leisure":       {"cinema", " game", "plaza", "beer hug"},
+		"transport":     {"uber", "taxify", "lyft", "uberm"},
+		"restaurant":    {"cafe neo", "neo", "cafe"},
+		"grocery":       {"hubmart", "spar", "domino"},
+		"savings":       {"piggybank", "cowrywise", "piggy"},
+		"personal":      {"cosmetics"},
+		"loans":         {"paylater", "branch", "aella", "onefinance", "onefi"},
+		"entertainment": {"netflix", "iroko", "iflix", "googleplay", "ecenter", "center"},
+		"online":        {"paystack", "flutterwave"},
+		"atm":           {"atm", "withdrawal"},
+		"transfer":      {"cr"},
+	}
+)
